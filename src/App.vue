240eab5e9ffee49d7a7f4d92c8a2c420511a468c
Monica <mymunoznava@gmail.com>
2019-06-20 20:16:46 -0500

    firts animation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
       <router-link to="/projects">Projects</router-link>
     </div>
-    <router-view/>
+    <transition
+      name="router-anim"
+      enter-active-class="animated fadeInLeft"
+      leave-active-class="animated fadeOutLeft"
+    >
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <style lang="scss">
+@import './assets/animate.css@3.5.1.css';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
